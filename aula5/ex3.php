88849ea2b94d0a31337ad0398eaeb34e8e32d9ec
raissa <r.tenorio@aluno.ifsp.edu.br>
2022-03-23 11:05:01 -0300

    exemplo3

---
+++ aula5/ex3.php
@@ -0,0 +1,11 @@
+<?php
+    $op= 'a';
+        switch($op){
+            case 'a': echo "Abrir"; break;
+            case 'b': echo "Fechar"; break;
+            case 'c': echo "Salvar"; break;
+            case 'd': echo "Sair"; break;
+        }
+
+
+?>

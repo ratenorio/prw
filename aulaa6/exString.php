09c832c67c4cd45c09b6935fff1efebfa51f9067
raissa <r.tenorio@aluno.ifsp.edu.br>
2022-03-30 10:18:48 -0300

    exemplostring

---
+++ aulaa6/exString.php
@@ -0,0 +1,8 @@
+<?php
+$palavra = "  Ciência da computação   ";
+echo "A variável contém " .strlen($palavra) . " Caracteres" . "<br>";
+$palavra =trim($palavra);
+echo "A variável agora contém " . strlen($palavra) . " caracteres";
+
+
+?>

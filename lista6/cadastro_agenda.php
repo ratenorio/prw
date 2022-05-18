a478ebd77073db627fd22151e0d0b03ec3c6b0f7
raissa <r.tenorio@aluno.ifsp.edu.br>
2022-05-18 11:56:29 -0300

    AGENDA

--- lista6/cadastro_agenda.php
+++ lista6/cadastro_agenda.php
@@ -1,17 +1,29 @@
 <?php
     include("conexao.php");
-    $nome_usuario= $_POST["txtNome"];
-    $email_usuario= $_POST["txtEmail"];
-    $fone_usuario= $_POST["txtTel"];
-
+    $nome_usuario= $_POST["nome"];
+    $apelido_usuario= $_POST["apelido"];
+    $endereco= $_POST["endereco"];
+    $bairro= $_POST["bairro"];
+    $cidade_usuario= $_POST["cidade"];
+    $estado_usuario= $_POST["estado"];
+    $telefone_usuario= $_POST["telefone"];
+    $celular_usuario= $_POST["celular"];
+    $email_usuario= $_POST["email"];
+  
     echo "<P>Nome: " .$nome_usuario. "<br>";
-    echo "E-mail: " .$email_usuario ."<br>";
-    echo "Telefone: " .$fone_usuario ."</P>";
+    echo "Apelido: " .$apelido_usuario ."<br>";
+    echo "Endereco: " .$endereco. "<br>";
+    echo "Bairro: " .$bairro ."<br>";
+    echo "Cidade: " .$cidade_usuario ."<br>";
+    echo "Estado: " .$estado_usuario. "<br>";
+    echo "Telefone: " .$telefone_usuario ."<br>";
+    echo "Celular: " .$celular_usuario. "<br>";
+    echo "Email: " .$email_usuario ."</P>";
 
-    $sql= "INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario)
-           VALUES  (' " .$nome_usuario." ', ' ".$email_usuario." ',' ".$fone_usuario. " ')";
+    $sql= "INSERT INTO agenda (nome, apelido, endereco, bairro, cidade, estado, telefone, celular, email)
+           VALUES  ('" .$nome_usuario."', '".$apelido_usuario."','".$endereco."', '".$bairro."','".$cidade_usuario."', '".$estado_usuario."', '".$telefone_usuario."', '".$celular_usuario."', '".$email_usuario. "')";
 
-    $result= mysqli_query ($con, $sql);
+    $result= mysqli_query($con, $sql);
     if($result)
         echo "Dados inseridos com sucesso";
     else

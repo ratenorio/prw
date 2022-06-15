e308ac1b3dd8604c3029543365633535fe66e5de
raissa <r.tenorio@aluno.ifsp.edu.br>
2022-06-15 09:38:37 -0300

    prova2

--- prova2/consulta_fluxo_caixa.php
+++ prova2/consulta_fluxo_caixa.php
@@ -1,10 +1,6 @@
 <?php
     include ('conexao.php');
-    $sql= 'SELECT * FROM fluxo_caixa';
-    //retorna todos os dados da consulta
-    $result= mysqli_query($con, $sql);
-    //retorna a primeira linha
-    //$row= mysqli_fetch_array($result);
+  
     $tipo= $_POST["tipo"];
 
 ?>
@@ -19,21 +15,22 @@
     <h1>Consulta: </h1>
     
     <?php 
-    while($row= mysqli_fetch_array($result)){
+    echo " " .$tipo;
     if($tipo=='entrada'){
         $sql="select sum(valor) valor from fluxo_caixa where tipo= 'entrada'";
-        echo " ".$row['tipo'];
+       
         
     }else if($tipo=='saida'){
-        $sql="select sum(valor) valor from fluxo_caixa where tipo= 'saida'";
-        echo " ".$row['tipo'];
+        $sql="select sum(valor) valor from fluxo_caixa where tipo= 'saida'";       
    
     }else if($tipo=='saldo'){
         $sql= "select sum(case when tipo='entrada' then valor else 0 end) -
-            sum(case when tipo= 'saida' then valor else 0 end) as valor from fluxo caixa";
-            echo " ".$row['tipo'];
+            sum(case when tipo= 'saida' then valor else 0 end) as valor from fluxo_caixa";           
     }
-}
+    $result= mysqli_query($con, $sql);
+    $row= mysqli_fetch_array($result);
+    echo " ".$row['valor'];
+
     ?>
 </body>
 </html>
